60ed55ce174a81e1b7e219d0229ab71f7c71c41a
Peter Ejiofor <tercrft@gmail.com>
2025-08-25 23:06:44 +0100

    Working on basic layout of info page

--- pechie/src/components/Page.tsx
+++ pechie/src/components/Page.tsx
@@ -3,9 +3,13 @@ import Body from "./Body";
 
 const Page = () =>{
     return(
-        <div className="mx-[90px]">
-            <Header/>
-            <Body/>
+        <div className="min-h-screen flex flex-col">
+            <div className="mx-[90px]">
+                <Header/>
+                <div className="max-w-[1075px] mx-auto pb-[50px]" >
+                    <Body/>
+                </div>
+            </div>
         </div>
     )
 }
--- pechie/src/components/pages/Info.tsx
+++ pechie/src/components/pages/Info.tsx
@@ -1,6 +1,102 @@
 const Info = () =>{
     return(
         <>
+
+        <div className="flex flex-col gap-15">
+            <section className="flex flex-row justify-center border border-black w-full">
+
+                <article className="flex flex-1 justify-start border border-black">
+                    <div className="w-[489px] h-[341px] bg-black rounded-xl">
+
+                    </div>
+                </article>
+
+                <article className="flex flex-1 justify-end border border-black">
+                    <div className="w-[227px] h-[341px] bg-black ">
+
+                    </div>
+                </article>
+
+
+            </section>
+
+            <section className="flex border border-black h-[66px]">
+                <h2>Skills</h2>
+
+            </section>
+
+            <section className="flex flex-col  h-auto gap-5">
+                <h2>Experience</h2>
+
+                <article className="flex flex-col gap-5 p-[20px] border border-black rounded-xl">
+
+                    <div>
+                    
+                    <p>
+                        <strong>Junior Front-End Developer</strong><br/>
+                        Arch Arcades | May 2025 - July 2025<br/>
+                    </p>
+                    <ul>
+                        <li><p>Conducted UX research through user interviews and defined the overall project vision</p></li>
+                        <li><p>Produced wireframes and mockups that maintained team focus and guided project progress.</p></li>
+                        <li><p>Implemented an interactive prototype in Figma to support community-driven feedback.</p></li>
+                        <li><p>Led frontend development using React and TailwindCSS, modernizing the site’s look and feel.</p></li>
+                        <li><p>Integrated a backend server to fetch essential data from an arcade games API.</p></li>
+                    </ul>
+                    
+                    </div>
+                    <div>
+                                        <p>
+                        <strong>Junior Front-End Developer</strong><br/>
+                        Arch Arcades | May 2025 - July 2025<br/>
+                    </p>
+                    <ul>
+                        <li><p>Conducted UX research through user interviews and defined the overall project vision</p></li>
+                        <li><p>Produced wireframes and mockups that maintained team focus and guided project progress.</p></li>
+                        <li><p>Implemented an interactive prototype in Figma to support community-driven feedback.</p></li>
+                        <li><p>Led frontend development using React and TailwindCSS, modernizing the site’s look and feel.</p></li>
+                        <li><p>Integrated a backend server to fetch essential data from an arcade games API.</p></li>
+                    </ul>
+                    </div>
+                </article>
+            </section>
+
+            <section className="flex border border-black h-auto">
+                <h2>Education/Certificates</h2>
+                                <article className="flex flex-col gap-5 p-[20px] border border-black rounded-xl">
+
+                    <div>
+                    
+                    <p>
+                        <strong>Junior Front-End Developer</strong><br/>
+                        Arch Arcades | May 2025 - July 2025<br/>
+                    </p>
+                    <ul>
+                        <li><p>Conducted UX research through user interviews and defined the overall project vision</p></li>
+                        <li><p>Produced wireframes and mockups that maintained team focus and guided project progress.</p></li>
+                        <li><p>Implemented an interactive prototype in Figma to support community-driven feedback.</p></li>
+                        <li><p>Led frontend development using React and TailwindCSS, modernizing the site’s look and feel.</p></li>
+                        <li><p>Integrated a backend server to fetch essential data from an arcade games API.</p></li>
+                    </ul>
+                    
+                    </div>
+                    <div>
+                                        <p>
+                        <strong>Junior Front-End Developer</strong><br/>
+                        Arch Arcades | May 2025 - July 2025<br/>
+                    </p>
+                    <ul>
+                        <li><p>Conducted UX research through user interviews and defined the overall project vision</p></li>
+                        <li><p>Produced wireframes and mockups that maintained team focus and guided project progress.</p></li>
+                        <li><p>Implemented an interactive prototype in Figma to support community-driven feedback.</p></li>
+                        <li><p>Led frontend development using React and TailwindCSS, modernizing the site’s look and feel.</p></li>
+                        <li><p>Integrated a backend server to fetch essential data from an arcade games API.</p></li>
+                    </ul>
+                    </div>
+                </article>
+                
+            </section>
+        </div>
         </>
     );
 }
