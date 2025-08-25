f55b2e08d9be596a3188364a1151fd50ff46107c
Peter Ejiofor <tercrft@gmail.com>
2025-08-25 20:46:20 +0100

    Added a more interactive feel to the deploy button

--- pechie/src/components/Button.tsx
+++ pechie/src/components/Button.tsx
@@ -1,4 +1,7 @@
-type ButtonType = "primary" | "secondary"
+import { useState } from "react";
+
+
+type ButtonType = "primary" | "secondary" | "dark"
 
 
 interface ButtonProps{
@@ -9,14 +12,28 @@ interface ButtonProps{
 
 const Button = ({text="", type="primary"}:ButtonProps) =>{
 
+
+    const [displayText, setDisplayText] = useState(text);
+
+    const onEnter = () =>{
+        setDisplayText(prev => prev + " " + "↗")
+    }
+
     const colorClass: Record<ButtonType, string> = {
         primary: "bg-[var(--primary)] text-white",
-        secondary: "bg-white text-black"
+        secondary: "bg-white text-black border border-black",
+        dark: "bg-black text-white"
+    }
+
+        const colorClassHover: Record<ButtonType, string> = {
+        primary: "hover:bg-white hover:text-black hover:border hover:border-black",
+        secondary: "hover:bg-black hover:text-white",
+        dark: "hover:bg-white hover:text-black hover:border hover:border-black"
     }
     return(
         <>
-            <button className={`${colorClass[type]} w-[118px] h-[29px] rounded-xl`}>
-                {`${text?text:"Default"}`}
+            <button onMouseEnter={onEnter} onMouseLeave={() => setDisplayText(text)} className={`${colorClass[type]} w-[118px] hover:w-[146px] h-[29px] rounded-xl ${colorClassHover[type]} transition-all duration-[300ms]`}>
+                {displayText}
             </button>
         </>
     )
