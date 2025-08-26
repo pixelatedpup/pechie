c9347e4e4af2356c6ba5412a740b559554b2e6ab
Peter Ejiofor <tercrft@gmail.com>
2025-08-26 20:18:11 +0100

    Fixed info page error with padding/margin issues

--- pechie/src/components/pages/Info.tsx
+++ pechie/src/components/pages/Info.tsx
@@ -61,38 +61,38 @@ const Info = () =>{
                 </article>
             </section>
 
-            <section className="flex border border-black h-auto">
+            <section className="flex flex-col gap-5  h-auto">
                 <h2>Education/Certificates</h2>
-                                <article className="flex flex-col gap-5 p-[20px] border border-black rounded-xl">
+                    <article className="flex flex-col gap-5 p-[20px] border border-black rounded-xl">
 
-                    <div>
-                    
-                    <p>
-                        <strong>Junior Front-End Developer</strong><br/>
-                        Arch Arcades | May 2025 - July 2025<br/>
-                    </p>
-                    <ul>
-                        <li><p>Conducted UX research through user interviews and defined the overall project vision</p></li>
-                        <li><p>Produced wireframes and mockups that maintained team focus and guided project progress.</p></li>
-                        <li><p>Implemented an interactive prototype in Figma to support community-driven feedback.</p></li>
-                        <li><p>Led frontend development using React and TailwindCSS, modernizing the site’s look and feel.</p></li>
-                        <li><p>Integrated a backend server to fetch essential data from an arcade games API.</p></li>
-                    </ul>
-                    
-                    </div>
-                    <div>
-                                        <p>
-                        <strong>Junior Front-End Developer</strong><br/>
-                        Arch Arcades | May 2025 - July 2025<br/>
-                    </p>
-                    <ul>
-                        <li><p>Conducted UX research through user interviews and defined the overall project vision</p></li>
-                        <li><p>Produced wireframes and mockups that maintained team focus and guided project progress.</p></li>
-                        <li><p>Implemented an interactive prototype in Figma to support community-driven feedback.</p></li>
-                        <li><p>Led frontend development using React and TailwindCSS, modernizing the site’s look and feel.</p></li>
-                        <li><p>Integrated a backend server to fetch essential data from an arcade games API.</p></li>
-                    </ul>
-                    </div>
+                        <div>
+                        
+                            <p>
+                                <strong>Junior Front-End Developer</strong><br/>
+                                Arch Arcades | May 2025 - July 2025<br/>
+                            </p>
+                            <ul>
+                                <li><p>Conducted UX research through user interviews and defined the overall project vision</p></li>
+                                <li><p>Produced wireframes and mockups that maintained team focus and guided project progress.</p></li>
+                                <li><p>Implemented an interactive prototype in Figma to support community-driven feedback.</p></li>
+                                <li><p>Led frontend development using React and TailwindCSS, modernizing the site’s look and feel.</p></li>
+                                <li><p>Integrated a backend server to fetch essential data from an arcade games API.</p></li>
+                            </ul>
+                        
+                        </div>
+                        <div>
+                            <p>
+                                <strong>Junior Front-End Developer</strong><br/>
+                                Arch Arcades | May 2025 - July 2025<br/>
+                            </p>
+                            <ul>
+                                <li><p>Conducted UX research through user interviews and defined the overall project vision</p></li>
+                                <li><p>Produced wireframes and mockups that maintained team focus and guided project progress.</p></li>
+                                <li><p>Implemented an interactive prototype in Figma to support community-driven feedback.</p></li>
+                                <li><p>Led frontend development using React and TailwindCSS, modernizing the site’s look and feel.</p></li>
+                                <li><p>Integrated a backend server to fetch essential data from an arcade games API.</p></li>
+                            </ul>
+                        </div>
                 </article>
                 
             </section>
