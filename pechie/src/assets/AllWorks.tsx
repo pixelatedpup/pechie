33ce24b30950e152cdfa83e92c01117063637f29
Peter Ejiofor <tercrft@gmail.com>
2025-09-01 20:31:14 +0100

    Added a description for each project. Hoping to add images next

--- pechie/src/assets/AllWorks.tsx
+++ pechie/src/assets/AllWorks.tsx
@@ -4,7 +4,7 @@ export const works = [
         name:"Luna", 
         deploy:"https://github.com/pixelatedpup/lunacomics", 
         tags:["TypeScript", "React", "HTML/CSS", "TailWindCSS", "ExpressJS"], 
-        description:"Lorem ipsum dolor sit amet. Aut possimus consequatur ab omnis nostrum qui optio saepe aut exercitationem facilis qui dolorem officiis et omnis quia aut earum quia. Non consequuntur distinctio ad deserunt numquam ut minima dolor id unde esse! Non alias facilis et facere rerum ea doloribus saepe. Et numquam ducimus ad quibusdam omnis rem eveniet obcaecati.",
+        description:"A website to help independent comic authors post their comics, update their fans and sell their merchandise.",
         github:"https://github.com/pixelatedpup/lunacomics",
         figma:"",
         images:[{cover:"", first:"", second:"", third:""}]
@@ -15,7 +15,7 @@ export const works = [
         name:"Arch Arcades", 
         deploy:"https://pixelatedpup.github.io/archarcades", 
         tags:["JavaScript", "React", "HTML/CSS", "TailWindCSS"], 
-        description:"Lorem ipsum dolor sit amet. Aut possimus consequatur ab omnis nostrum qui optio saepe aut exercitationem facilis qui dolorem officiis et omnis quia aut earum quia. Non consequuntur distinctio ad deserunt numquam ut minima dolor id unde esse! Non alias facilis et facere rerum ea doloribus saepe. Et numquam ducimus ad quibusdam omnis rem eveniet obcaecati.",
+        description:"A website for a local arcade that displays each game using a library API, aswell as the current highscore of each member. Currently in production ,but core features have been implemented..",
         github:"https://github.com/pixelatedpup/archarcades",
         figma:"https://www.figma.com/design/BnrdiFaGesFvgOKNbqN135/Arch-Arcades?node-id=2-148&t=h6mjttNQwib45vOQ-1",
         images:[{cover:"", first:"", second:"", third:""}]
@@ -27,7 +27,7 @@ export const works = [
         name:"OGIUS", 
         deploy:"https://github.com/pixelatedpup/ogius", 
         tags:["TypeScript", "React", "HTML", "TailWindCSS", "MongoDB"], 
-        description:"Lorem ipsum dolor sit amet. Aut possimus consequatur ab omnis nostrum qui optio saepe aut exercitationem facilis qui dolorem officiis et omnis quia aut earum quia. Non consequuntur distinctio ad deserunt numquam ut minima dolor id unde esse! Non alias facilis et facere rerum ea doloribus saepe. Et numquam ducimus ad quibusdam omnis rem eveniet obcaecati.",
+        description:"A portfolio website to display my illustrations, commision services and shop items. A prototype has been made in figma, but actual coding has not began implementation.",
         github:"https://github.com/pixelatedpup/ogius",
         figma:"https://www.figma.com/design/ZLceXblPLH4lB0ytC5TLrd/OGIUS?t=h6mjttNQwib45vOQ-1",
         images:[{cover:"", first:"", second:"", third:""}]
@@ -39,7 +39,7 @@ export const works = [
         name:"Sunset Cinemas", 
         deploy:"sample", 
         tags:["Figma"], 
-        description:"Lorem ipsum dolor sit amet. Aut possimus consequatur ab omnis nostrum qui optio saepe aut exercitationem facilis qui dolorem officiis et omnis quia aut earum quia. Non consequuntur distinctio ad deserunt numquam ut minima dolor id unde esse! Non alias facilis et facere rerum ea doloribus saepe. Et numquam ducimus ad quibusdam omnis rem eveniet obcaecati.",
+        description:"A mobile application that allows movie enthusiasts to order snacks and accumulate credits to help with discounted prices.",
         github:"",
         figma:"https://www.figma.com/design/WOFTb63byDA1zXHgfyuEkS/Sunset-Cinemas?node-id=46-194&t=h6mjttNQwib45vOQ-1",
         images:[{cover:"", first:"", second:"", third:""}]
@@ -51,7 +51,7 @@ export const works = [
         name:"PECHIEDEV(OLD)", 
         deploy:"https://github.com/pixelatedpup/pechiedev", 
         tags:["JavaScript", "React", "HTML/CSS"], 
-        description:"Lorem ipsum dolor sit amet. Aut possimus consequatur ab omnis nostrum qui optio saepe aut exercitationem facilis qui dolorem officiis et omnis quia aut earum quia. Non consequuntur distinctio ad deserunt numquam ut minima dolor id unde esse! Non alias facilis et facere rerum ea doloribus saepe. Et numquam ducimus ad quibusdam omnis rem eveniet obcaecati.",
+        description:"My old portfolio website to diplay past programming projects.",
         github:"https://github.com/pixelatedpup/pechiedev",
         figma:"",
         images:[{cover:"", first:"", second:"", third:""}]
